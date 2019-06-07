2522f07f59f78fcf8f3710ffafc0a4a7032a8d13
Thomas Lee <thomasslee97@gmail.com>
2019-06-07 16:08:49 +0100

    Load FULL videos from spreadsheet

--- src/blueprint0/getSegment.ts
+++ src/blueprint0/getSegment.ts
@@ -5,6 +5,7 @@ import {
 } from 'tv-automation-sofie-blueprints-integration'
 import { literal } from '../common/util'
 import { SourceLayer } from '../types/layers'
+import { Piece } from '../types/classes'
 
 export function getSegment (context: SegmentContext, ingestSegment: IngestSegment): BlueprintResultSegment {
 	const segment = literal<IBlueprintSegment>({
@@ -24,24 +25,42 @@ export function getSegment (context: SegmentContext, ingestSegment: IngestSegmen
 				context.warning(`Missing type for part: '${part.name || part.externalId}'`)
 				parts.push(createGeneric(part))
 			} else if (type.match(/full/i)) {
-				// TODO
-				console.log('hi!')
-				parts.push(createGeneric(part))
+				console.log('Creating a full part')
+				let pieces: IBlueprintPiece[] = []
+				if ('pieces' in part.payload) {
+					(part.payload['pieces'] as Piece[]).forEach(piece => {
+						if (piece.objectType === 'video') {
+							let p = literal<IBlueprintPiece>({
+								_id: '',
+								externalId: piece.id,
+								name: piece.clipName,
+								enable: { start: piece.objectTime, end: piece.objectTime + piece.duration },
+								outputLayerId: 'pgm0',
+								sourceLayerId: SourceLayer.PgmClip
+							})
+							pieces.push(p)
+						}
+
+					})
+				} else {
+					console.log(`No pieces in part '${part.name}'`)
+				}
+				parts.push(createPart(part, pieces))
 			} else if (type.match(/cam/i)) {
 				// TODO
-				console.log('hi!')
+				console.log('hi! Got a cam part!')
 				parts.push(createGeneric(part))
 			} else if (type.match(/ls/i)) {
 				// TODO
-				console.log('hi!')
+				console.log('hi! Got an ls part!')
 				parts.push(createGeneric(part))
 			} else if (type.match(/split/i)) {
 				// TODO
-				console.log('hi!')
+				console.log('hi! Got a split part!')
 				parts.push(createGeneric(part))
 			} else if (type.match(/head/i)) {
 				// TODO
-				console.log('hi!')
+				console.log('hi! Got a head part!')
 				parts.push(createGeneric(part))
 			} else {
 				context.warning(`Missing type '${type}' for part: '${part.name || part.externalId}'`)
@@ -69,7 +88,7 @@ function createGeneric (ingestPart: IngestPart): BlueprintResultPart {
 		_id: '',
 		externalId: ingestPart.externalId,
 		name: part.title,
-		enable: { start: 0, duration: 0 },
+		enable: { start: 0, duration: 100 },
 		outputLayerId: 'pgm0',
 		sourceLayerId: SourceLayer.PgmCam
 	})
@@ -79,4 +98,39 @@ function createGeneric (ingestPart: IngestPart): BlueprintResultPart {
 		adLibPieces: [],
 		pieces: [piece]
 	}
+}
+
+/**
+ * Creates a part from an ingest part and associated pieces.
+ * @param {IngestPart} ingestPart Ingest part.
+ * @param {IBlueprintPiece[]} pieces Array of pieces.
+ */
+function createPart (ingestPart: IngestPart, pieces: IBlueprintPiece[]): BlueprintResultPart {
+	const part = literal<IBlueprintPart>({
+		externalId: ingestPart.externalId,
+		title: ingestPart.name || 'Unknown',
+		metaData: {},
+		typeVariant: '',
+		expectedDuration: calculateExpectedDuration(pieces)
+	})
+
+	return {
+		part,
+		adLibPieces: [],
+		pieces: pieces
+	}
+}
+
+/**
+ * Calculates the expected duration of a part from component pieces.
+ * @param {IBlueprintPiece[]} pieces Pieces to calculate duration for.
+ */
+function calculateExpectedDuration (pieces: IBlueprintPiece[]): number {
+	let duration = 0
+
+	pieces.forEach(piece => {
+		duration += (piece.enable.duration as number) // This will get more complicated as more rules are added
+	})
+
+	return duration
 }
---
+++ src/types/classes.ts
@@ -0,0 +1,9 @@
+export interface Piece {
+	id: string,
+	objectType: string, // TODO: Enum?
+	objectTime: number,
+	duration: number,
+	clipName: string,
+	attributes: any,
+	position: string
+}
