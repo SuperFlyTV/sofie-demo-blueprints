9b892735f3e6077a831b3576833b3484678f6bc3
Thomas Lee <thomasslee97@gmail.com>
2019-09-20 11:10:24 +0100

    feat: Placeholder parts

--- src/blueprint0/getSegment.ts
+++ src/blueprint0/getSegment.ts
@@ -1,11 +1,13 @@
 import * as _ from 'underscore'
+import * as objectPath from 'object-path'
 import {
-	SegmentContext, IngestSegment, BlueprintResultSegment, IBlueprintSegment, BlueprintResultPart
+	SegmentContext, IngestSegment, BlueprintResultSegment, IBlueprintSegment, BlueprintResultPart, IngestPart, IBlueprintPart, IBlueprintPiece
 } from 'tv-automation-sofie-blueprints-integration'
 import { literal } from '../common/util'
 import { SegmentConf } from '../types/classes'
 import { parseConfig } from './helpers/config'
 import { parseSources } from './helpers/sources'
+import { SourceLayer } from '../types/layers'
 
 export function getSegment (context: SegmentContext, ingestSegment: IngestSegment): BlueprintResultSegment {
 	const config: SegmentConf = {
@@ -30,8 +32,53 @@ export function getSegment (context: SegmentContext, ingestSegment: IngestSegmen
 		}
 	}
 
+	for (const part of ingestSegment.parts) {
+		if (!part.payload) {
+			context.warning(`Missing payload for part: '${part.name || part.externalId}'`)
+		} else if (part.payload['float']) {
+			continue
+		} else {
+			const type = objectPath.get(part.payload, 'type', '') + ''
+			if (!type) {
+				context.warning(`Missing type for part: '${part.name || part.externalId}'`)
+				parts.push(createGeneric(part))
+			} else {
+				parts.push(createGeneric(part))
+			}
+		}
+	}
+
 	return {
 		segment,
 		parts
+	}
+}
+
+/**
+ * Creates a generic part. Only used as a placeholder for part types that have not been implemented yet.
+ * @param {Piece} piece Piece to evaluate.
+ */
+function createGeneric (ingestPart: IngestPart): BlueprintResultPart {
+	const part = literal<IBlueprintPart>({
+		externalId: ingestPart.externalId,
+		title: ingestPart.name || 'Unknown',
+		metaData: {},
+		typeVariant: '',
+		expectedDuration: 5000
+	})
+
+	const piece = literal<IBlueprintPiece>({
+		_id: '',
+		externalId: ingestPart.externalId,
+		name: part.title,
+		enable: { start: 0, duration: 100 },
+		outputLayerId: 'pgm0',
+		sourceLayerId: SourceLayer.PgmCam
+	})
+
+	return {
+		part,
+		adLibPieces: [],
+		pieces: [piece]
 	}
 }
