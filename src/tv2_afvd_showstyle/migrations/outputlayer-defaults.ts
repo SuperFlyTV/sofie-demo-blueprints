d323efaa273ad390832bd24dfda1d49e9ec1a0b6
Thomas Lee <thomasslee97@gmail.com>
2019-10-04 12:42:14 +0100

    chore: Remove output layers inherited from spreadsheet blueprints

--- src/tv2_afvd_showstyle/migrations/outputlayer-defaults.ts
+++ src/tv2_afvd_showstyle/migrations/outputlayer-defaults.ts
@@ -7,29 +7,5 @@ export default literal<IOutputLayer[]>([
 		name: 'PGM',
 		isPGM: true,
 		_rank: 0
-	},
-	{
-		_id: 'monitor0',
-		name: 'BAK',
-		isPGM: false,
-		_rank: 1
-	},
-	{
-		_id: 'screen1',
-		name: 'Screen 1',
-		isPGM: false,
-		_rank: 2
-	},
-	{
-		_id: 'screen2',
-		name: 'Screen 2',
-		isPGM: false,
-		_rank: 2
-	},
-	{
-		_id: 'screen3',
-		name: 'Screen 3',
-		isPGM: false,
-		_rank: 2
 	}
 ])
