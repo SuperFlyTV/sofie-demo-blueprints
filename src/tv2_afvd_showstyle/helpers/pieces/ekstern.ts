fc625aaa6a1c681098ec70d09974db454bbd99ef
Thomas Lee <thomasslee97@gmail.com>
2019-10-11 10:32:28 +0100

    chore: Break ekstern parser into own file

---
+++ src/tv2_afvd_showstyle/helpers/pieces/ekstern.ts
@@ -0,0 +1,153 @@
+import {
+	AtemTransitionStyle,
+	DeviceType,
+	TimelineContentTypeAtem,
+	TimelineContentTypeSisyfos,
+	TimelineObjAtemME,
+	TimelineObjSisyfosMessage
+} from 'timeline-state-resolver-types'
+import {
+	IBlueprintPiece,
+	PartContext,
+	PieceLifespan,
+	RemoteContent,
+	SourceLayerType,
+	TimelineObjectCoreExt
+} from 'tv-automation-sofie-blueprints-integration'
+import { literal } from '../../../common/util'
+import { BlueprintConfig } from '../../../tv2_afvd_studio/helpers/config'
+import { FindSourceInfoStrict } from '../../../tv2_afvd_studio/helpers/sources'
+import { AtemLLayer, SisyfosSourceRemote } from '../../../tv2_afvd_studio/layers'
+import {
+	CueDefinitionEkstern,
+	CueTime,
+	CueType,
+	ParseCue,
+	UnparsedCue
+} from '../../inewsConversion/converters/ParseCue'
+import { SourceLayer } from '../../layers'
+import { CalculateTime } from './evaluateCues'
+
+export function EvaluateEkstern(
+	context: PartContext,
+	config: BlueprintConfig,
+	pieces: IBlueprintPiece[],
+	partId: string,
+	parsedCue: CueDefinitionEkstern
+) {
+	const eksternProps = ((parsedCue as unknown) as CueDefinitionEkstern).source.match(/^LIVE ([^\s]+)(?: (.+))?$/i)
+	if (!eksternProps) {
+		return
+	}
+	const source = eksternProps[1]
+	const variant = eksternProps[2]
+	if (!source) {
+		return
+	}
+	const sourceInfoCam = FindSourceInfoStrict(context, config.sources, SourceLayerType.REMOTE, (parsedCue as any).source)
+	if (sourceInfoCam === undefined) {
+		return
+	}
+	const atemInput = sourceInfoCam.port
+	pieces.push(
+		literal<IBlueprintPiece>({
+			_id: '',
+			externalId: partId,
+			name: eksternProps[0],
+			enable: {
+				start: parsedCue.start ? CalculateTime(parsedCue.start) : 0,
+				...(parsedCue.end ? { end: CalculateTime(parsedCue.end) } : {})
+			},
+			outputLayerId: 'pgm0',
+			sourceLayerId: SourceLayer.PgmLive,
+			infiniteMode: PieceLifespan.OutOnNextPart,
+			content: literal<RemoteContent>({
+				studioLabel: '',
+				switcherInput: atemInput,
+				timelineObjects: literal<TimelineObjectCoreExt[]>([
+					literal<TimelineObjAtemME>({
+						id: '',
+						enable: {
+							start: 0
+						},
+						priority: 1,
+						layer: AtemLLayer.AtemMEProgram,
+						content: {
+							deviceType: DeviceType.ATEM,
+							type: TimelineContentTypeAtem.ME,
+							me: {
+								input: atemInput,
+								transition: AtemTransitionStyle.CUT // TODO: This may change
+							}
+						}
+					}),
+
+					...(variant
+						? variant.match(/stereo/gi)
+							? [
+									literal<TimelineObjSisyfosMessage>({
+										id: '',
+										enable: {
+											start: 0
+										},
+										priority: 1,
+										layer: SisyfosSourceRemote(source, 'stereo_1'),
+										content: {
+											deviceType: DeviceType.SISYFOS,
+											type: TimelineContentTypeSisyfos.SISYFOS,
+											isPgm: 1,
+											faderLevel: 0.75
+										}
+									}),
+									literal<TimelineObjSisyfosMessage>({
+										id: '',
+										enable: {
+											start: 0
+										},
+										priority: 1,
+										layer: SisyfosSourceRemote(source, 'stereo_2'),
+										content: {
+											deviceType: DeviceType.SISYFOS,
+											type: TimelineContentTypeSisyfos.SISYFOS,
+											isPgm: 1,
+											faderLevel: 0.75
+										}
+									})
+							  ]
+							: [
+									literal<TimelineObjSisyfosMessage>({
+										id: '',
+										enable: {
+											start: 0
+										},
+										priority: 1,
+										layer: SisyfosSourceRemote(source, variant),
+										content: {
+											deviceType: DeviceType.SISYFOS,
+											type: TimelineContentTypeSisyfos.SISYFOS,
+											isPgm: 1,
+											faderLevel: 0.75
+										}
+									})
+							  ]
+						: [
+								literal<TimelineObjSisyfosMessage>({
+									id: '',
+									enable: {
+										start: 0
+									},
+									priority: 1,
+									layer: SisyfosSourceRemote(source),
+									content: {
+										deviceType: DeviceType.SISYFOS,
+										type: TimelineContentTypeSisyfos.SISYFOS,
+										isPgm: 1,
+										faderLevel: 0.75
+									}
+								})
+						  ])
+				])
+			})
+		})
+	)
+}
--- src/tv2_afvd_showstyle/helpers/pieces/evaluateCues.ts
+++ src/tv2_afvd_showstyle/helpers/pieces/evaluateCues.ts
@@ -1,31 +1,7 @@
-import {
-	AtemTransitionStyle,
-	DeviceType,
-	TimelineContentTypeAtem,
-	TimelineContentTypeSisyfos,
-	TimelineObjAtemME,
-	TimelineObjSisyfosMessage
-} from 'timeline-state-resolver-types'
-import {
-	IBlueprintPiece,
-	PartContext,
-	PieceLifespan,
-	RemoteContent,
-	SourceLayerType,
-	TimelineObjectCoreExt
-} from 'tv-automation-sofie-blueprints-integration'
-import { literal } from '../../../common/util'
+import { IBlueprintPiece, PartContext } from 'tv-automation-sofie-blueprints-integration'
 import { BlueprintConfig } from '../../../tv2_afvd_studio/helpers/config'
-import { FindSourceInfoStrict } from '../../../tv2_afvd_studio/helpers/sources'
-import { AtemLLayer, SisyfosSourceRemote } from '../../../tv2_afvd_studio/layers'
-import {
-	CueDefinitionEkstern,
-	CueTime,
-	CueType,
-	ParseCue,
-	UnparsedCue
-} from '../../inewsConversion/converters/ParseCue'
-import { SourceLayer } from '../../layers'
+import { CueTime, CueType, ParseCue, UnparsedCue } from '../../inewsConversion/converters/ParseCue'
+import { EvaluateEkstern } from './ekstern'
 
 const FRAME_TIME = 1000 / 25 // TODO: This should be pulled from config.
 
@@ -39,137 +15,9 @@ export function EvaluateCues(
 	cues.forEach(cue => {
 		if (cue) {
 			const parsedCue = ParseCue(cue)
-			switch (
-				parsedCue.type // TODO: Break out into individual files
-			) {
+			switch (parsedCue.type) {
 				case CueType.Ekstern:
-					console.log(`CUE: ${JSON.stringify(parsedCue)}`)
-					const eksternProps = ((parsedCue as unknown) as CueDefinitionEkstern).source.match(
-						/^LIVE ([^\s]+)(?: (.+))?$/i
-					)
-					console.log(`Props: ${eksternProps}`)
-					if (!eksternProps) {
-						break
-					}
-					const source = eksternProps[1]
-					const variant = eksternProps[2]
-					if (!source) {
-						break
-					}
-					console.log(`Source: ${source}`)
-					console.log(`Variant: ${variant}`)
-					const sourceInfoCam = FindSourceInfoStrict(
-						context,
-						config.sources,
-						SourceLayerType.REMOTE,
-						(parsedCue as any).source
-					)
-					if (sourceInfoCam === undefined) {
-						break
-					}
-					const atemInput = sourceInfoCam.port
-					pieces.push(
-						literal<IBlueprintPiece>({
-							_id: '',
-							externalId: partId,
-							name: eksternProps[0],
-							enable: {
-								start: parsedCue.start ? calculateTime(parsedCue.start) : 0,
-								...(parsedCue.end ? { end: calculateTime(parsedCue.end) } : {})
-							},
-							outputLayerId: 'pgm0',
-							sourceLayerId: SourceLayer.PgmLive,
-							infiniteMode: PieceLifespan.OutOnNextPart,
-							content: literal<RemoteContent>({
-								studioLabel: '',
-								switcherInput: atemInput,
-								timelineObjects: literal<TimelineObjectCoreExt[]>([
-									literal<TimelineObjAtemME>({
-										id: '',
-										enable: {
-											start: 0
-										},
-										priority: 1,
-										layer: AtemLLayer.AtemMEProgram,
-										content: {
-											deviceType: DeviceType.ATEM,
-											type: TimelineContentTypeAtem.ME,
-											me: {
-												input: atemInput,
-												transition: AtemTransitionStyle.CUT // TODO: This may change
-											}
-										}
-									}),
-
-									...(variant
-										? variant.match(/stereo/gi)
-											? [
-													literal<TimelineObjSisyfosMessage>({
-														id: '',
-														enable: {
-															start: 0
-														},
-														priority: 1,
-														layer: SisyfosSourceRemote(source, 'stereo_1'),
-														content: {
-															deviceType: DeviceType.SISYFOS,
-															type: TimelineContentTypeSisyfos.SISYFOS,
-															isPgm: 1,
-															faderLevel: 0.75
-														}
-													}),
-													literal<TimelineObjSisyfosMessage>({
-														id: '',
-														enable: {
-															start: 0
-														},
-														priority: 1,
-														layer: SisyfosSourceRemote(source, 'stereo_2'),
-														content: {
-															deviceType: DeviceType.SISYFOS,
-															type: TimelineContentTypeSisyfos.SISYFOS,
-															isPgm: 1,
-															faderLevel: 0.75
-														}
-													})
-											  ]
-											: [
-													literal<TimelineObjSisyfosMessage>({
-														id: '',
-														enable: {
-															start: 0
-														},
-														priority: 1,
-														layer: SisyfosSourceRemote(source, variant),
-														content: {
-															deviceType: DeviceType.SISYFOS,
-															type: TimelineContentTypeSisyfos.SISYFOS,
-															isPgm: 1,
-															faderLevel: 0.75
-														}
-													})
-											  ]
-										: [
-												literal<TimelineObjSisyfosMessage>({
-													id: '',
-													enable: {
-														start: 0
-													},
-													priority: 1,
-													layer: SisyfosSourceRemote(source),
-													content: {
-														deviceType: DeviceType.SISYFOS,
-														type: TimelineContentTypeSisyfos.SISYFOS,
-														isPgm: 1,
-														faderLevel: 0.75
-													}
-												})
-										  ])
-								])
-							})
-						})
-					)
-					console.log(`Pieces: ${JSON.stringify(pieces)}`)
+					EvaluateEkstern(context, config, pieces, partId, parsedCue)
 					break
 				default:
 					break
@@ -178,7 +26,7 @@ export function EvaluateCues(
 	})
 }
 
-function calculateTime(time: CueTime) {
+export function CalculateTime(time: CueTime) {
 	let result = 0
 	if (time.seconds) {
 		result += time.seconds * 1000
