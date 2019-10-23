de676802552f96252d3e3ef23fefb0895e93f96d
Thomas Lee <thomasslee97@gmail.com>
2019-10-23 14:50:55 +0100

    fix: Failing test

--- src/tv2_afvd_showstyle/__tests__/layers-check.ts
+++ src/tv2_afvd_showstyle/__tests__/layers-check.ts
@@ -43,7 +43,7 @@ export function checkAllLayers(
 		...getCameraSisyfosMappings(
 			'1:1,2:2,3:3,4:4,5:5,1S:1,2S:1,3S:3,4S:4,5S:5,CS 1:1,CS 2:2,CS 3:3,CS 4:4,CS 5:5,CS 6:6,AR:7,HVID:8,1S:1,3S:3,X8:8,11:11,12:12,13:13,14:14,15:15,SORT:1,CS1:1,CS2:2,CS3:3,CS4:4,CS5:5'
 		),
-		...getRemoteSisyfosMappings('1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8'),
+		...getRemoteSisyfosMappings('1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8,9:9,10:10'),
 		...getSkypeSisyfosMappings('1:1,2:2,3:3,4:4,5:5,6:6,7:7,8:8'),
 		...getTelefonSisyfosMappings('TLF 2:1')
 	})
