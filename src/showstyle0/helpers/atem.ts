163dbb6b921ea9f9f511911a7b64117e7b7779e9
Balte de Wit <contact@balte.nl>
2021-11-05 07:29:44 +0100

    feat: enable atem transitions

--- src/showstyle0/helpers/atem.ts
+++ src/showstyle0/helpers/atem.ts
@@ -1,10 +1,15 @@
-import { TSR } from '@sofie-automation/blueprints-integration'
+import { TimelineObjectCoreExt, TSR } from '@sofie-automation/blueprints-integration'
 import { literal } from '../../common/util'
 import { AtemLayers } from '../../studio0/layers'
 
-export function createAtemInputTimelineObjects(input: number, start = 0): TSR.TimelineObjAtemME[] {
+export function createAtemInputTimelineObjects(
+	input: number,
+	start = 0,
+	transitionDuration = 40,
+	transitionProps?: Omit<TSR.TimelineObjAtemME['content']['me'], 'programInput' | 'previewInput'>
+): TSR.TimelineObjAtemME[] {
 	return [
-		literal<TSR.TimelineObjAtemME>({
+		literal<TimelineObjectCoreExt & TSR.TimelineObjAtemME>({
 			id: '',
 			enable: { start: start },
 			layer: AtemLayers.AtemMeProgram,
@@ -16,22 +21,22 @@ export function createAtemInputTimelineObjects(input: number, start = 0): TSR.Ti
 					programInput: input,
 				},
 			},
-			// TODO - keyframes will be needed to do transitions but breaks preview for now
-			// keyframes: [
-			//     {
-			//         id: '',
-			//         enable: {
-			//             start: 0,
-			//             duration: 1000 // only used to do the transition
-			//         },
-			//         content: {
-			//             me: {
-			//                 input: input,
-			//                 transition: TSR.AtemTransitionStyle.CUT
-			//             }
-			//         }
-			//     }
-			// ]
+			keyframes: [
+				{
+					id: '',
+					enable: {
+						start: 0,
+						duration: transitionDuration, // only used to do the transition
+					},
+					content: {
+						me: literal<TSR.TimelineObjAtemME['content']['me']>({
+							input: input,
+							transition: TSR.AtemTransitionStyle.CUT,
+							...(transitionProps || {}),
+						}),
+					},
+				},
+			],
 		}),
 		// Add object for preview
 		literal<TSR.TimelineObjAtemME>({
@@ -43,9 +48,23 @@ export function createAtemInputTimelineObjects(input: number, start = 0): TSR.Ti
 				type: TSR.TimelineContentTypeAtem.ME,
 
 				me: {
-					previewInput: input,
+					previewInput: 0,
 				},
 			},
+			keyframes: [
+				{
+					id: '',
+					enable: {
+						start: transitionDuration + 40, // after the transition keyframe
+					},
+					content: {
+						me: {
+							previewInput: input,
+						},
+					},
+					preserveForLookahead: true,
+				} as any, // todo
+			],
 		}),
 	]
 }
