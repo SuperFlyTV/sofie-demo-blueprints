e72e27d05d8c99ab79fd86fb922078ce54c863aa
olzzon <github@olzzon.dk>
2024-06-11 08:18:38 +0200

    fix: StudioblueprintManifest - configPreset and applyconfig - update blueprint integration

--- packages/blueprints/src/studio0/index.ts
+++ packages/blueprints/src/studio0/index.ts
@@ -8,13 +8,14 @@ import { getShowStyleId } from './getShowStyleId'
 // import { studioMigrations } from './migrations'
 import ConfigSchema = require('./config-schema.json')
 import DefaultMappings, { AtemMappings } from './migrations/mappings-defaults'
+import { StudioConfig } from '../generated/studio-config'
 
 declare const VERSION: string // Injected by webpack
 declare const VERSION_TSR: string // Injected by webpack
 declare const VERSION_INTEGRATION: string // Injected by webpack
 declare const TRANSLATION_BUNDLES: string // injected by webpack
 
-const manifest: StudioBlueprintManifest = {
+const manifest: StudioBlueprintManifest<StudioConfig> = {
 	blueprintType: BlueprintManifestType.STUDIO,
 
 	blueprintId: 'sofie-generic-studio',
@@ -27,6 +28,9 @@ const manifest: StudioBlueprintManifest = {
 		default: {
 			name: 'Default',
 			config: {
+				previewRenderer: 'sofie',
+				casparcgLatency: 0,
+				visionMixerType: 'atem',
 				sisyfosSources: {},
 				vmixSources: {},
 				atemOutputs: {},
@@ -44,6 +48,7 @@ const manifest: StudioBlueprintManifest = {
 			...AtemMappings,
 		},
 		playoutDevices: {},
+		routeSets: {},
 	}),
 
 	getBaseline,
