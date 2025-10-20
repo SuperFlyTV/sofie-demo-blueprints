5dcabd441a3efb45f51e3b808bd7abc7e49e69ee
Robert (Jamie) Munro <rjmunro@arjam.net>
2025-10-20 02:19:09 +0100

    Allow retiming of graphics pieces

---
+++ packages/blueprints/src/base/showstyle/helpers/userEditOperations.ts
@@ -0,0 +1,18 @@
+import {
+	DefaultUserOperationsTypes,
+	UserEditingType,
+	UserEditingDefinition,
+} from '@sofie-automation/blueprints-integration'
+
+/**
+ * Creates standard user edit operations for pieces that support retiming
+ * @returns Array of user edit operations including retime functionality
+ */
+export function createPieceUserEditOperations(): UserEditingDefinition[] {
+	return [
+		{
+			type: UserEditingType.SOFIE,
+			id: DefaultUserOperationsTypes.RETIME_PIECE,
+		},
+	]
+}
