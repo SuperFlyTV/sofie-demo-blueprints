e8c6e2a17d62bdde63634ed7464477f3b635a32c
Robert (Jamie) Munro <rjmunro@arjam.net>
2025-11-24 14:46:17 +0000

    Add some default atemSources
    So that the user doesn't have to configure these manually.

--- packages/blueprints/src/main/studio/configs/demo.ts
+++ packages/blueprints/src/main/studio/configs/demo.ts
@@ -1,4 +1,4 @@
-import { StudioConfig, VisionMixerDevice } from '../../../base/studio/helpers/config.js'
+import { SourceType, StudioConfig, VisionMixerDevice } from '../../../base/studio/helpers/config.js'
 
 export const DemoStudioConfig: StudioConfig = {
 	previewRenderer: 'sofie',
@@ -10,12 +10,25 @@ export const DemoStudioConfig: StudioConfig = {
 		deviceId: 'atem0',
 	},
 	audioMixer: {
-		host: '0.0.0.0',
+		host: 'localhost',
 		port: 1176,
 		deviceId: 'sisyfos0',
+	},
+	casparcg: {
+		host: 'localhost',
+		port: 5250,
 	},
 	sisyfosSources: {},
 	vmixSources: {},
 	atemOutputs: {},
-	atemSources: {},
+	atemSources: {
+		camera1: { input: 1, type: SourceType.Camera },
+		camera2: { input: 2, type: SourceType.Camera },
+		camera3: { input: 3, type: SourceType.Camera },
+		camera4: { input: 4, type: SourceType.Camera },
+		remote1: { input: 5, type: SourceType.Remote },
+		remote2: { input: 6, type: SourceType.Remote },
+		mediaplayer: { input: 7, type: SourceType.MediaPlayer },
+		graphics: { input: 8, type: SourceType.Graphics },
+	},
 }
