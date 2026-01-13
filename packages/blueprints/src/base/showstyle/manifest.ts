17227c755b5c637a42754ef417f812fee599e578
Robert (Jamie) Munro <rjmunro@arjam.net>
2026-01-13 21:06:54 +0000

    Add comments to showstyle manifest

--- packages/blueprints/src/base/showstyle/manifest.ts
+++ packages/blueprints/src/base/showstyle/manifest.ts
@@ -18,28 +18,54 @@ import * as ConfigSchema from '../../$schemas/main-showstyle-config.json'
 import { dereferenceSync } from 'dereference-json-schema'
 
 export const baseManifest: Omit<ShowStyleBlueprintManifest<ShowStyleConfig>, 'blueprintId' | 'configPresets'> = {
+	/** The type of this blueprint */
 	blueprintType: BlueprintManifestType.SHOWSTYLE,
 
+	/** Version of this blueprint */
 	blueprintVersion: __VERSION__,
+	/** Version of blueprints-integration that this blueprint depends on */
 	integrationVersion: __VERSION_INTEGRATION__,
+	/** Version of timeline-state-resolver-types that this blueprint depends on */
 	TSRVersion: __VERSION_TSR__,
 
+	/** A list of config items this blueprint expects to be available on the ShowStyle */
 	showStyleConfigSchema: JSONBlobStringify<JSONSchema>(
 		dereferenceSync(JSON.parse(JSON.stringify(ConfigSchema))) as any
 	),
 
+	/** Returns the id of the show style variant to use for a rundown, return null to ignore that rundown */
 	getShowStyleVariantId,
+	/** Generate rundown from ingest data. return null to ignore that rundown */
 	getRundown,
+	/** Generate segment from ingest data */
 	getSegment,
+	/** Generate adlib piece from ingest data */
 	getAdlibItem,
+	/** Execute an action defined by an IBlueprintActionManifest */
 	executeAction,
+	/**
+	 * Execute an action defined by an IBlueprintActionManifest.
+	 *
+	 * This callback allows an action to perform operations only on the Timeline Datastore.
+	 * This allows for a _fast-path_ for rapid-fire actions, before the full `executeAction` callback resolves.
+	 */
 	executeDataStoreAction,
 
+	/** Called just before `onTimelineGenerate` to perform AB-playback for the timeline */
 	getAbResolverConfiguration,
 	// translations: __TRANSLATION_BUNDLES__,
 
+	/**
+	 * Validate the config passed to this blueprint.
+	 * Do various sanity checks of the config and return a list of messages to display to the user.
+	 */
 	validateConfig,
+	/**
+	 * Apply the config by generating the data to be saved into the db.
+	 * This should be written to give a predictable and stable result, it can be called with the same config multiple times.
+	 */
 	applyConfig,
+	/** Called when a RundownPlaylist has been activated */
 	onRundownActivate: async (_context: IRundownActivationContext) => {
 		// Noop
 	},
