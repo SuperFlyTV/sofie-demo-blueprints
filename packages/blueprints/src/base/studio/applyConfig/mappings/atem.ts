2e3fd4b8b99719415e481fd975dfed8dc33fdafe
Julian Waller <julian@superfly.tv>
2026-04-28 10:43:53 +0100

    feat: generate all needed mappings (#131)

---
+++ packages/blueprints/src/base/studio/applyConfig/mappings/atem.ts
@@ -0,0 +1,62 @@
+import { BlueprintMappings, BlueprintMapping, TSR, LookaheadMode } from '@sofie-automation/blueprints-integration'
+import { literal } from '../../../../common/util.js'
+import { BlueprintConfig, OutputConfig } from '../../helpers/config.js'
+import { AtemLayers } from '../../layers.js'
+
+export function getAtemMappings(config: BlueprintConfig): BlueprintMappings {
+	const mappings: BlueprintMappings = {
+		[AtemLayers.AtemMeProgram]: literal<BlueprintMapping<TSR.MappingAtemMixEffect>>({
+			device: TSR.DeviceType.ATEM,
+			deviceId: 'atem0',
+			lookahead: LookaheadMode.NONE,
+
+			options: { mappingType: TSR.MappingAtemType.MixEffect, index: 0 },
+		}),
+		[AtemLayers.AtemMePreview]: literal<BlueprintMapping<TSR.MappingAtemMixEffect>>({
+			device: TSR.DeviceType.ATEM,
+			deviceId: 'atem0',
+			lookahead: LookaheadMode.PRELOAD,
+
+			options: { mappingType: TSR.MappingAtemType.MixEffect, index: 0 },
+		}),
+		[AtemLayers.AtemDskGraphics]: literal<BlueprintMapping<TSR.MappingAtemDownStreamKeyer>>({
+			device: TSR.DeviceType.ATEM,
+			deviceId: 'atem0',
+			lookahead: LookaheadMode.NONE,
+
+			options: { mappingType: TSR.MappingAtemType.DownStreamKeyer, index: 0 },
+		}),
+		[AtemLayers.AtemSuperSourceProps]: literal<BlueprintMapping<TSR.MappingAtemSuperSourceProperties>>({
+			device: TSR.DeviceType.ATEM,
+			deviceId: 'atem0',
+			lookahead: LookaheadMode.NONE,
+
+			options: { mappingType: TSR.MappingAtemType.SuperSourceProperties, index: 0 },
+		}),
+		[AtemLayers.AtemSuperSourceBoxes]: literal<BlueprintMapping<TSR.MappingAtemSuperSourceBox>>({
+			device: TSR.DeviceType.ATEM,
+			deviceId: 'atem0',
+			lookahead: LookaheadMode.WHEN_CLEAR,
+			lookaheadMaxSearchDistance: 1,
+
+			options: { mappingType: TSR.MappingAtemType.SuperSourceBox, index: 0 },
+		}),
+	}
+
+	for (const output of Object.values<OutputConfig>(config.studio.atemOutputs)) {
+		const i = output.output - 1
+		if (isNaN(i) || i < 0) continue
+
+		mappings[`atem_aux_${i}`] = literal<BlueprintMapping<TSR.MappingAtemAuxilliary>>({
+			device: TSR.DeviceType.ATEM,
+			deviceId: 'atem0',
+			lookahead: LookaheadMode.NONE,
+			options: {
+				mappingType: TSR.MappingAtemType.Auxilliary,
+				index: i,
+			},
+		})
+	}
+
+	return mappings
+}
--- packages/blueprints/src/base/studio/applyConfig/mappings/index.ts
+++ packages/blueprints/src/base/studio/applyConfig/mappings/index.ts
@@ -1,10 +1,36 @@
-import { BlueprintMappings } from '@sofie-automation/blueprints-integration'
-import { BlueprintConfig } from '../../helpers/config.js'
-import mappingsDefaults from './mappings-defaults.js'
+import { BlueprintMappings, ICommonContext, LookaheadMode, TSR } from '@sofie-automation/blueprints-integration'
+import { BlueprintConfig, VisionMixerDevice } from '../../helpers/config.js'
+import { getVMixMappings } from './vmix.js'
+import { getAtemMappings } from './atem.js'
+import { getSisyfosMappings } from './sisyfos.js'
+import { getCasparCGMappings } from './casparcg.js'
+import { AbstractLayers } from '../../layers.js'
+import { assertNever } from '../../../../common/util.js'
 
-export function getMappingsDefaults(config: BlueprintConfig): BlueprintMappings {
-	console.log('getMappingsDefaults', config)
-	//ToDo: Split this up into files with specific mappings for each device:
-	const mappings: BlueprintMappings = mappingsDefaults
+export function getMappingsDefaults(context: ICommonContext, config: BlueprintConfig): BlueprintMappings {
+	const mappings: BlueprintMappings = {
+		[AbstractLayers.CoreAbstract]: {
+			device: TSR.DeviceType.ABSTRACT,
+			deviceId: 'abstract0',
+			lookahead: LookaheadMode.NONE,
+			options: {},
+		},
+		...getSisyfosMappings(config),
+		...getCasparCGMappings(config),
+	}
+
+	switch (config.studio.visionMixer.type) {
+		case VisionMixerDevice.Atem:
+			Object.assign(mappings, getAtemMappings(config))
+			break
+		case VisionMixerDevice.VMix:
+			Object.assign(mappings, getVMixMappings(config.studio.vmixSources))
+			break
+		default:
+			assertNever(config.studio.visionMixer.type)
+			context.logError('Unknown vision mixer type: ' + config.studio.visionMixer.type)
+			break
+	}
+
 	return mappings
 }
